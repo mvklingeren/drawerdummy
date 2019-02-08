c7cfeaa61b194c9a3f7ad3a16503ce3413d01279
Milco van Klingeren <mvklingeren@intersoftware.nl>
2019-02-08 07:27:45 +0100

    sample

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Drawer } from "devextreme-react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Menu() {
+  return (
+    <div className="App">
+      <p>Menu</p>
+      <Link to="/dummy1">Dummy1</Link>
+      <Link to="/dummy2">Dummy2</Link>
+    </div>
+  );
 }
 
-export default App;
+function Dummy1() {
+  return (
+    <div className="App">
+      <h1>Hi im a dummy #1</h1>
+    </div>
+  );
+}
+
+function Dummy2() {
+  return (
+    <div className="App">
+      <h1>Hi im dummy number Two</h1>
+    </div>
+  );
+}
+
+export function AppNotWorking() {
+  return (
+    <BrowserRouter>
+      <Drawer opened component={Menu}>
+        {/* HERE routes inside drawer */}
+        <Route path="/dummy1/" component={Dummy1} />
+        <Route path="/dummy2/" component={Dummy2} />
+      </Drawer>
+    </BrowserRouter>
+  );
+}
+
+export function AppWorking() {
+  return (
+    <BrowserRouter>
+      <>
+        <Drawer opened component={Menu} />
+        {/* HERE routes outside drawer */}
+        <Route path="/dummy1/" component={Dummy1} />
+        <Route path="/dummy2/" component={Dummy2} />
+      </>
+    </BrowserRouter>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<AppWorking />, rootElement);
